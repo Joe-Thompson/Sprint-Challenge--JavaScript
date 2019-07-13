fed5d4d237a03ef57bc1fb1fe785cd1c09e9a6d9
joe.thompson3696 <bmo3696@gmail.com>
2019-07-12 20:15:22 -0400

    functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume (x, y, cb) {
+  return cb(x, y);
+};
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,12 +16,23 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (x, y) {
+  console.log(x + y);
+};
+
+function multiply (x, y) {
+  console.log(x * y);
+};
+
+function greeting (first, last) {
+  console.log("Hello " + first + " " + last + ", nice to meet you!");
+ };
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +40,12 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+
+/* 
+'nestedfunction()' can access the variable 'internal' because it is inside of the function 'myFunction', closure allows us to reach from inside our nested function
+scope outwards to grab elements to use inside, however we can not access any elements outside of a function that have not been returned by said function.
+Our compiler on it's first pass knows about 'myFunction' but does not know what is inside of 'myFunction' until it is called in our
+ */
 
 
 const external = "I'm outside the function";
